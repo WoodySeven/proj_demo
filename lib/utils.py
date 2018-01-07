4877d36fc7e423312db26e6e6ff3709f904a515c
viprs <viprs@qq.com>
2018-01-07 11:26:52 +0800

    测试用例的添加

---
+++ lib/utils.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# author: samren
+import shutil
+import os
+
+shutil.copyfile("HTMLTestRunner.py", "c:\HTMLTestRunner.py")
+
+os.system("del c:\\HTMLTestRunner.py")
--- testcases/cases_login_logout/admin_login_logout.py
+++ testcases/cases_login_logout/admin_login_logout.py
@@ -6,6 +6,7 @@ from selenium import webdriver
 class Bugfree管理员登录退出(unittest.TestCase):
     """
     演示的是bugfree的登录和退出
+    数据驱动，相同的逻辑使用不同的数据去运行。
     """
     def setUp(self):
         self.driver = webdriver.Chrome()
@@ -16,7 +17,7 @@ class Bugfree管理员登录退出(unittest.TestCase):
     def tearDown(self):
         pass
 
-    def test_admin_login(self):
+    def test_admin_login_success(self):
         """admin的登录正确用例"""
         driver = self.driver
         driver.get(self.base_url + "/bugfree/index.php/site/login")
@@ -28,6 +29,17 @@ class Bugfree管理员登录退出(unittest.TestCase):
         driver.find_element_by_id("SubmitLoginBTN").click()
         time.sleep(3)
 
+    def test_admin_login_fail_with_invalid_username(self):
+        """admin的登录失败，使用无效的用户名"""
+        driver = self.driver
+        driver.get(self.base_url + "/bugfree/index.php/site/login")
+        driver.find_element_by_id("LoginForm_username").clear()
+        driver.find_element_by_id("LoginForm_username").send_keys("invalid")
+        driver.find_element_by_id("LoginForm_password").clear()
+        driver.find_element_by_id("LoginForm_password").send_keys("123456")
+        driver.find_element_by_id("LoginForm_rememberMe").click()
+        driver.find_element_by_id("SubmitLoginBTN").click()
+        time.sleep(3)
 
 if __name__ == '__main__':
     unittest.main()
