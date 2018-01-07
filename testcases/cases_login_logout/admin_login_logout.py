29f256555c1f1422c6da0891872be0ee587d93c2
viprs <viprs@qq.com>
2018-01-07 11:46:24 +0800

    使用ddt做测试用例的数据驱动

--- testcases/cases_login_logout/admin_login_logout.py
+++ testcases/cases_login_logout/admin_login_logout.py
@@ -1,8 +1,14 @@
 import unittest
 import time
+import ddt
 from selenium import webdriver
 
 
+test_data = [['admin', '123456', '退出'],
+             ['invalid', '123456', '用户名不存在'],
+             ['', '123456', '不可为空白']]
+
+@ddt.ddt
 class Bugfree管理员登录退出(unittest.TestCase):
     """
     演示的是bugfree的登录和退出
@@ -17,29 +23,21 @@ class Bugfree管理员登录退出(unittest.TestCase):
     def tearDown(self):
         pass
 
-    def test_admin_login_success(self):
-        """admin的登录正确用例"""
+    @ddt.unpack
+    @ddt.data(*test_data)
+    def test_admin_login_test(self, admin, password, flag):
+        """admin的登录的所有测试用例"""
         driver = self.driver
         driver.get(self.base_url + "/bugfree/index.php/site/login")
         driver.find_element_by_id("LoginForm_username").clear()
-        driver.find_element_by_id("LoginForm_username").send_keys("admin")
+        driver.find_element_by_id("LoginForm_username").send_keys(admin)
         driver.find_element_by_id("LoginForm_password").clear()
-        driver.find_element_by_id("LoginForm_password").send_keys("123456")
+        driver.find_element_by_id("LoginForm_password").send_keys(password)
         driver.find_element_by_id("LoginForm_rememberMe").click()
         driver.find_element_by_id("SubmitLoginBTN").click()
         time.sleep(3)
+        self.assertIn(flag, driver.page_source)
 
-    def test_admin_login_fail_with_invalid_username(self):
-        """admin的登录失败，使用无效的用户名"""
-        driver = self.driver
-        driver.get(self.base_url + "/bugfree/index.php/site/login")
-        driver.find_element_by_id("LoginForm_username").clear()
-        driver.find_element_by_id("LoginForm_username").send_keys("invalid")
-        driver.find_element_by_id("LoginForm_password").clear()
-        driver.find_element_by_id("LoginForm_password").send_keys("123456")
-        driver.find_element_by_id("LoginForm_rememberMe").click()
-        driver.find_element_by_id("SubmitLoginBTN").click()
-        time.sleep(3)
 
 if __name__ == '__main__':
     unittest.main()
